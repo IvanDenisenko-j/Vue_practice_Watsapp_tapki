d8d0003e74aca83ef77c521fa47a55718dd422fc
IvanDenisenko-j <wanius89039158476@gmail.com>
2025-02-11 17:08:37 +0700

    9 точно

--- js/main.js
+++ js/main.js
@@ -140,7 +140,11 @@ let app = new Vue({
             this.cart.push(id);
         },
         delCart(id) {
-            this.cart.pop(id);
+            for (var i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                    this.cart.splice(i, 1);
+                }
+            }
         }
     }
 })
